a5349df39f67d70b00a626138415d31c8918d1f5
Bannikov Igor <Virgil1234@yandex.ru>
2022-12-18 00:54:13 +0300

    доделал

---
+++ src/test/java/SecondCaseTest.java
@@ -0,0 +1,20 @@
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
+import io.restassured.specification.RequestSpecification;
+import org.junit.jupiter.api.Test;
+import pojo.ProductData;
+
+public class SecondCaseTest {
+    @Test
+    public void fsdafsdfasfda(){
+        Specifications specifications = new Specifications();
+        ProductData product = new ProductData("test product",(float)10);
+        RequestSpecification requestSpec= specifications.getStoreRequestSpec(product);
+        RestAssured.requestSpecification = requestSpec;
+        RestAssured.responseSpecification = specifications.getResponseSpec();
+        Response response = RestAssured.post("products/");
+        //response.prettyPrint();
+        response.then()
+                .statusCode(200);
+    }
+}
